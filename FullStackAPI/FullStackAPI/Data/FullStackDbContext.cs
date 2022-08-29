7b6e7ec04bcb317115feb85879cbdc51d13e3aba
Thevapriyan07 <thevapriyan65@gmail.com>
2022-08-29 21:12:47 +0530

    Initial commit

---
+++ FullStackAPI/FullStackAPI/Data/FullStackDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FullStackAPI.models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStackAPI.Data
+{
+    public class FullStackDbContext : DbContext
+    {
+        public FullStackDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; } //property
+    }
+}
---
+++ FullStackAPI/FullStackAPI/Migrations/20220828155007_Initial Migration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FullStackAPI.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    phone = table.Column<long>(type: "bigint", nullable: false),
+                    salary = table.Column<long>(type: "bigint", nullable: false),
+                    department = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
